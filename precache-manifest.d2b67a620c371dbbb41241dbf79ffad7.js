84ae9a251ce7b7f67d65388bee86e1693b2508e4
Nikky6666 <borbut.nika@mail.ru>
2019-11-27 14:00:56 +0300

    Updates

--- precache-manifest.d2b67a620c371dbbb41241dbf79ffad7.js
+++ precache-manifest.d2b67a620c371dbbb41241dbf79ffad7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a26c6cf3eb587230fd696393ac9931c",
+    "revision": "8ccb98ea784ab0c2851c74c16982bd3f",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "5ad65c022d37ea6a9c29",
-    "url": "/portfolio/static/css/main.9256162c.chunk.css"
+    "revision": "50bf776188b250764b3c",
+    "url": "/portfolio/static/css/main.295efb63.chunk.css"
   },
   {
     "revision": "b01f4d8e8d97b18fbc88",
     "url": "/portfolio/static/js/2.218523ca.chunk.js"
   },
   {
-    "revision": "5ad65c022d37ea6a9c29",
-    "url": "/portfolio/static/js/main.91ea0251.chunk.js"
+    "revision": "50bf776188b250764b3c",
+    "url": "/portfolio/static/js/main.a40b6956.chunk.js"
   },
   {
     "revision": "76aa8cbd67b5c937ec2c",
